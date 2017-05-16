6f51db469f82c2d3789ae7053ee83e6af436c1e2
Nic Ryan <nryan@timelio.net.au>
2017-05-16 17:00:38 +1000

    js basic functionality

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(function() {
+  $("p").hide();
+
+  $("#h1").click(function() {
+    $("#p1").toggle();
+  });
+
+  $("#h2").click(function() {
+    $("#p2").toggle();
+  });
+
+  $("#h3").click(function() {
+    $("#p3").toggle();
+  });
+
+  $("#h4").click(function() {
+    $("#p4").toggle();
+  });
+
+});
