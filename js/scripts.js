46a42ae811f627aa79cf19f8922792cf78f4d713
Nic Ryan <nryan@timelio.net.au>
2017-05-16 17:23:38 +1000

    way shorter and smarter way to do it

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,8 @@
 $(function() {
   $("p").hide();
 
-  $("#h1").click(function() {
-    $("#p1").toggle();
-  });
-
-  $("#h2").click(function() {
-    $("#p2").toggle();
-  });
-
-  $("#h3").click(function() {
-    $("#p3").toggle();
-  });
-
-  $("#h4").click(function() {
-    $("#p4").toggle();
+  $("h4").click(function() {
+    $(this).next().toggle();
   });
 
 });
